ee1b1251a0ee674cc79f2c228a6178912973e624
saasbook <saasbook@saasbook.(none)>
2013-12-18 14:06:48 -0800

    corrected -0 option in xargs

--- tailme.sh
+++ tailme.sh
@@ -2,7 +2,7 @@
 
 if ! (ps -ef | grep "tail -F -n-0 typescript.txt" | grep -v grep > /dev/null); then
   echo "Start tailing typescript.txt for zeitgesit_event.py"
-  tail -F -n-0 typescript.txt | xargs -0 -I {} python zeitgeist_event.py {} &
+  tail -F -n-0 typescript.txt | xargs -r --delimiter="\n" -I {} python zeitgeist_event.py {} &
 else
   echo "Already tailing typescript"
 fi
